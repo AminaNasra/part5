f5cd05b11e0e25cf70139c75d1d47a2f888103b9
AminaNasra <nasraaar2020@gmail.com>
2025-06-16 14:19:50 +1000

    Added Movie Class declaration
    Declare Movie Class.
    Planned MovieList class

--- js/movieClass.js
+++ js/movieClass.js
@@ -1,1 +1,19 @@
+// file: movieClass.js
+// desc: This file defines the Movie class blueprint  encapsulating properties like: movieID, title, year, rating.
+//uses the Movie class and creates a movies array  based on Movie class,
 
+// Author: Amina Aar
+
+// Last modified: 16/06/2025
+// version 1.0.0
+
+// declare a Movie class
+class Movie {
+  // constructor
+  constructor(movieID, title, year, rating) {
+    this.movieID = movieID;
+    this.title = title;
+    this.year = year;
+    this.rating = rating;
+  } // end constructor
+} // end movie class
